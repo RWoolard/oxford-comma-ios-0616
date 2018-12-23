55a82d079d793c0848cae7b3d3de9513ed3d2f63
Rayshawn Woolard <r.woolard456@gmail.com>
2018-12-23 18:55:59 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,9 +4,6 @@ def oxford_comma(array)
   elsif array.length == 2
     return array.join(" and ")
   elsif array.length == 3
-    array.length - 2.times do
-      array.join(" ")
-    end
     array.insert(array.length - 1, "and")
     return array.join
   end
