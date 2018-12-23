5c71c22acbb9969eb31c65f7f15baa0a727dc887
Rayshawn Woolard <r.woolard456@gmail.com>
2018-12-23 19:11:42 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.length == 2
     return array.join(" and ")
   elsif array.length == 3
-    array[0..-2].join(", ")
     array.insert(array.length - 1, " and ")
+    return array[0..-2].join(", ")
   end
 end
